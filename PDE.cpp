9a1e950a5594c04bec99356d1ae6aa86ec1716ce
aflaverde <42219494+aflaverde@users.noreply.github.com>
2018-11-13 23:37:38 -0500

    Primer commit PDE

---
+++ PDE.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+	//Crea y abre el archivo de escritura de datos data_pde.dat
+	ofstream data_out;
+	data_out.open("data_pde.dat");
+	
+	
+}//MAIN
+/*Andres Felipe Laverde Martinez*/
