950c6c5da9f949a01030bf7e342dc42ed4356fcd
aflaverde <42219494+aflaverde@users.noreply.github.com>
2018-11-19 11:42:14 -0500

    Falta hacer correctamente rungekutta

--- ODE.cpp
+++ ODE.cpp
@@ -7,20 +7,44 @@ using namespace std;
 
 #define PI 3.14159
 
-//Aceleracion en x
-double acc_x(double g, double c, double m, double x0, double v0, double ang, double dt){
-	/* dvx/dt = -c/m(raiz(vx**2+vy**2)*vx)*/
-	double x=x0+v0*dt;
-	double v=v0+x/dt;
-	double vx=v*cos(ang);	//Vel. en x
-	double vy=v*sin(ang);	//Vel. en y
-	double a_x = -(c/m)*(sqrt(vx*vx + vy*vy)*vx); //Aceleracion en x
-	return a_x;
-}//acc_x
+//Posicion en x
+double pos_x(double g, double c, double m, double x0, double v0, double ang, double dt){
+	double v0x=v0*cos(ang*PI/180);
+	double x=(v0x/c)*(1-exp(-c*dt));
 
+	return x;
+}//pos_x
+
+//Posicion en y
+double pos_y(double g, double c, double m, double x0, double v0, double ang, double dt){
+	double v0y=v0*sin(ang*PI/180);
+	double y=(1/c)*(g/c+v0y)*(1-exp(-c*dt))-(g*dt/c);
+
+	return y;
+}//pos_y
+
+//Velocidad en x
+double vel_x(double g, double c, double m, double x0, double v0, double ang, double dt){
+	double v0x=v0*cos(ang*PI/180);
+	double vx=v0x*(exp(-c*dt));
+
+	return vx;
+}//vel_x
+
+//Velocidad en y
+double vel_y(double g, double c, double m, double x0, double v0, double ang, double dt){
+	double v0y=v0*cos(ang*PI/180);
+	double vy=(g/c+v0y)*(exp(-c*dt)-(g/c));
+
+	return vy;
+}//vel_y
+
+double rk4(double func, double g, double c, double m, double x0, double v0, double ang, double dt){
+}//rk4
+
+
+//--------------------MAIN-----------------------//
 int main(){	
-	double a_max;
-		
 	double x_pos[100];
 	double g=10.0;	//Gravedad
 	double c=0.2;	//Coef. de friccion
@@ -29,8 +53,6 @@ int main(){
 	double v0=300.0;//Velocidad inicial m/s
 	double ang=45*PI/180.0;	//Angulo en radianes
 	double dt=0.01;	//Delta de tiempo
-	
-	a_max=acc_x(g, c, m, x0, v0, ang, dt);
 	
 	FILE *output;      //Abre el archivo y guarda los datos en el archivo ode.dat
 	output = fopen("ode.dat","w");
