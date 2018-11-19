e66b3ab33f18286134a4c07d91b47dc780eeba8f
aflaverde <42219494+aflaverde@users.noreply.github.com>
2018-11-18 23:37:28 -0500

    Delete calor_plot.py

--- calor_plot.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-datos=np.genfromtxt("laplace-calor.dat", delimiter=',') #Importa los datos de laplace-calor y los grafica en una matriz bidimensional de forma m x n donde las filas m son el transcurso de tiempo y n son las columnas de la distancia x
-
-n=1000
-plt.imshow(datos[:n,:])	#Muestra una grafica con los primeros n instantes de tiempo
-plt.show()
